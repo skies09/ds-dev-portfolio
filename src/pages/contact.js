aab798f318f593d0bdf9364cef8293c37dc3eef3
Donna <skies09@hotmail.com>
2024-05-14 21:05:01 +0100

    contact page design updates

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,8 +1,10 @@
 import { useEffect, useRef, useState } from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
+import { motion } from "framer-motion";
 import * as Yup from "yup";
 // import emailjs from "@emailjs/browser";
-import { motion } from "framer-motion";
 
 export default function Contact() {
 	const containerRef = useRef(null);
@@ -54,21 +56,21 @@ export default function Contact() {
 				validationSchema={validationSchema}
 				onSubmit={handleSubmit}
 			>
-				<Form className="flex flex-col justify-start items-start w-10/12">
-					<p className="text-2xl text-colorFive font-bold flex justify-center text-center font-monoTwo">
+				<Form className="flex flex-col justify-start items-start w-full lg:w-10/12">
+					<p className="text-lg lg:text-2xl text-colorFive font-bold flex justify-center text-center font-monoTwo">
 						Name
 					</p>
 					<Field
-						className="w-full h-8 z-10 rounded-xl my-4"
+						className="w-full h-8 z-10 rounded-xl my-2 lg:my-4 pl-2"
 						type="text"
 						id="user_name"
 						name="user_name"
 					/>
-					<p className="text-2xl text-colorFive font-bold flex justify-center text-center font-monoTwo">
+					<p className="text-lg lg:text-2xl text-colorFive font-bold flex justify-center text-center font-monoTwo">
 						Email
 					</p>
 					<Field
-						className="w-full h-8 z-10 rounded-xl my-4"
+						className="w-full h-8 z-10 rounded-xl my-2 lg:my-4 pl-2"
 						type="email"
 						id="user_email"
 						name="user_email"
@@ -78,16 +80,20 @@ export default function Contact() {
 						name="user_email"
 						component="div"
 					/>
-					<p className="text-2xl text-colorFive font-bold flex justify-center text-center font-monoTwo">
+					<p className="text-lg lg:text-2xl text-colorFive font-bold flex justify-center text-center font-monoTwo">
 						Message
 					</p>
 					<Field
-						className="w-full h-36 z-10 rounded-xl my-4"
-						type="text"
+						className="w-full h-6 lg:h-36 z-10 rounded-xl my-2 lg:my-4 pl-2 pb-24"
+						type="textarea"
 						id="message"
 						name="message"
+						rows="3"
 					/>
-					<button className="flex justify-center items-center mx-auto px-4 py-2 bg-colorFour text-colorFive border-2 border-colorFive rounded-xl" type="submit">
+					<button
+						className="flex justify-center items-center mx-auto px-4 py-2 bg-colorFour text-colorFive border-2 border-colorFive rounded-xl"
+						type="submit"
+					>
 						Send
 					</button>
 				</Form>
@@ -105,16 +111,16 @@ export default function Contact() {
 				initial={{ opacity: 0, y: -100 }}
 				whileInView={{ opacity: 1, y: 0 }}
 				transition={{ duration: 2, ease: "easeOut", delay: 0.2 }}
-				className="pt-20 text-2xl text-colorFive font-bold flex justify-center text-center font-monoTwo"
+				className="pt-8 lg:pt-20 text-lg lg:text-2xl text-colorFive font-bold flex justify-center text-center font-monoTwo"
 			>
 				Got a cool project idea?
-				<br/>
+				<br />
 				Get in touch!
 			</motion.div>
-		
-			<div className="pt-8 flex flex-row justify-center items-center w-4/5 mx-auto">
+
+			<div className="pt-2 lg:pt-8 flex flex-col lg:flex-row justify-center items-center w-full lg:w-4/5 mx-auto">
 				<motion.div
-					className="w-1/2 flex justify-center items-start bg-colorThree h-96"
+					className="w-10/12 lg:w-1/2 flex justify-center items-start bg-colorThree h-80 lg:h-96"
 					initial={{ opacity: 0, x: -100 }}
 					whileInView={{ opacity: 1, x: 0 }}
 					transition={{ duration: 2, ease: "easeOut", delay: 0.2 }}
@@ -125,12 +131,34 @@ export default function Contact() {
 					)}
 				</motion.div>
 				<motion.div
-					className="w-1/2 flex justify-center items-center bg-colorFive h-96"
+					className="w-1/2 flex flex-col justify-center items-center h-52 lg:h-96"
 					initial={{ opacity: 0, x: 100 }}
 					whileInView={{ opacity: 1, x: 0 }}
 					transition={{ duration: 2, ease: "easeOut", delay: 0.2 }}
 				>
-					Contact
+					<div className="relative flex flex-col justify-start items-start border-b border-dotted border-colorFive">
+						<div className="flex flex-row items-center mb-4">
+							<FontAwesomeIcon
+								icon={faPhone}
+								size="md"
+								className="text-colorFive"
+							/>
+							<span className="ml-4 text-lg lg:text-2xl text-colorFive tracking-wider">
+								07950981097
+							</span>
+						</div>
+						<div className="flex flex-row items-center mb-6">
+							<FontAwesomeIcon
+								icon={faEnvelope}
+								size="md"
+								className="text-colorFive"
+							/>
+							<span className="ml-4 text-lg lg:text-2xl text-colorFive tracking-wider">
+								donna.smith09@hotmail.com
+							</span>
+						</div>
+					</div>
+
 				</motion.div>
 			</div>
 		</div>
