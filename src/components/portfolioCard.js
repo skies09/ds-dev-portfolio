9ea95b86101dc6f7c7ed751b51e36868242f79ec
Donna <skies09@hotmail.com>
2024-05-25 14:43:52 +0100

    portfolio card fixes

--- src/components/portfolioCard.js
+++ src/components/portfolioCard.js
@@ -1,112 +1,32 @@
 import React, { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-	faClose
-} from "@fortawesome/free-solid-svg-icons";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 
 export default function PortfolioCard({ project }) {
 	const [viewProject, setViewProject] = useState(false);
 
 	return (
-		<AnimatePresence>
-			{viewProject ? (
-				<motion.div
-					initial={{ opacity: 0, scale: 1 }}
-					animate={{ opacity: 1, scale: 1 }}
-					exit={{ opacity: 0, scale: 1 }}
-					transition={{ duration: 1, ease: "easeInOut" }}
-					className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50"
-					onClick={() => setViewProject(false)}
-				>
-					<motion.div
-						className="flex flex-col justify-center bg-colorTwo px-4 py-2 text-colorFive border border-colorFive rounded-xl w-5/6 md:w-1/2 lg:w-1/2 relative"
-						onClick={(e) => e.stopPropagation()}
-					>
-						<p className="text-2xl text-colorFive font-bold flex justify-center text-center font-sans py-2 tracking-widest">
-							{project.title}
-						</p>
-						<div className="flex justify-center items-center">
-							<img
-								className="w-4/6 h-auto border border-colorFive rounded-xl"
-								src={`../../assets/Images/${project.img}`}
-								alt="Project"
-							/>
-						</div>
-						<p className="text-2xl md:text-md text-colorFive font-bold flex justify-center text-center font-monoTwo pt-4">
-							{project.link}
-						</p>
-						<p className="ml-3 text-lg md:text-sm text-colorFive font-bold flex justify-start text-start font-serif tracking-wide py-2">
-							{project.longDescription}
-						</p>
+		<motion.div
+			className="flex flex-col justify-center items-center bg-colorTwo px-4 py-2 text-colorFive border border-colorFive rounded-xl w-5/6 md:w-1/2 lg:w-72 shadow-shadow-colorTwo"
+			initial={{ opacity: 1, scale: 1 }}
+			animate={{ rotateX: 0 }}
+			onClick={() => setViewProject(true)}
+		>
+			<p className="text-2xl text-colorFive font-bold flex justify-center text-center font-sans py-2 tracking-widest">
+				{project.title}
+			</p>
 
-						{project.keyFeatures &&
-							project.keyFeatures.length > 0 && (
-								<div className="ml-3">
-									<p className="text-sm text-colorFour font-bold flex justify-start font-sans">
-										Key features:
-									</p>
-									{project.keyFeatures.map((k, index) => (
-										<div
-											key={index}
-											className="flex flex-col justify-center items-start pb-2"
-										>
-											<p className="text-sm text-colorFive font-bold flex justify-center font-sans">
-												{k}
-											</p>
-										</div>
-									))}
-								</div>
-							)}
+			<img
+				className="w-5/6 lg:w-3/4 h-auto border border-colorFive rounded-xl"
+				src={`../../assets/Images/${project.img}`}
+				alt="Project"
+			/>
 
-						{project.languages && project.languages.length > 0 && (
-							<div className="ml-3 pt-2 flex flex-row justify-start items-start">
-								{project.languages.map((l, index) => (
-									<div
-										key={index}
-										className="mb-2 px-4 py-2 bg-colorThree text-colorFive border border-colorThree rounded-xl tracking-wider font-monoTwo mr-3"
-									>
-										<p className="text-sm text-colorFive font-bold flex justify-center font-sans">
-											{l}
-										</p>
-									</div>
-								))}
-							</div>
-						)}
-
-						<div
-							className="absolute top-0 right-0 cursor-pointer"
-							onClick={() => setViewProject(false)}
-						>
-						<FontAwesomeIcon icon={faClose} size="xl" className="text-colorFive hover:text-colorOne m-4" />
-						</div>
-					</motion.div>
-				</motion.div>
-			) : (
-				<motion.div
-					className="flex flex-col justify-center items-center bg-colorTwo px-4 py-2 text-colorFive border border-colorFive rounded-xl w-5/6 md:w-1/2 lg:w-72 shadow-shadow-colorTwo"
-					initial={{ opacity: 1, scale: 1}}
-					animate={{ rotateX: 0 }}
-					onClick={() => setViewProject(true)}
-				>
-					<p className="text-2xl text-colorFive font-bold flex justify-center text-center font-sans py-2 tracking-widest">
-						{project.title}
-					</p>
-
-					<img
-						className="w-5/6 lg:w-3/4 h-auto border border-colorFive rounded-xl"
-						src={`../../assets/Images/${project.img}`}
-						alt="Project"
-					/>
-
-					<p className="text-sm md:text-md text-colorFive font-bold flex justify-center text-center font-monoTwo pt-4">
-						{project.link}
-					</p>
-					<p className="text-sm md:text-sm text-colorFive font-bold flex justify-center text-center font-display tracking-wide py-4">
-						{project.shortDescription}
-					</p>
-				</motion.div>
-			)}
-		</AnimatePresence>
+			<p className="text-sm md:text-md text-colorFive font-bold flex justify-center text-center font-monoTwo pt-4">
+				{project.link}
+			</p>
+			<p className="text-sm md:text-sm text-colorFive font-bold flex justify-center text-center font-display tracking-wide py-4">
+				{project.shortDescription}
+			</p>
+		</motion.div>
 	);
 }
