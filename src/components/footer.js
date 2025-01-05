a983372b8aa63bc15133d6ec94f37e483c74588b
Donna <skies09@hotmail.com>
2025-01-05 15:06:13 +0000

    update footer designs

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,9 +2,13 @@ import React from "react";
 
 const Footer = () => {
 	return (
-		<div className="flex flex-row w-full justify-start items-center py-4 bg-colorThree border-2 border-t-colorFive">
-			<p className="text-colorFive text-sm pl-4">
-				© 2025 Donna Smith Portfolio. All rights reserved.
+		<div className="flex flex-row w-full justify-start items-center py-4 bg-gradient-to-b from-colorTwo to-colorThree border-t-2 border-dotted border-colorFive">
+			<p className="text-colorFive text-sm pl-4 z-10">
+				© 2025{" "}
+				<span className="font-bold hover:animate-wiggle">
+					Donna Smith
+				</span>{" "}
+				Portfolio. All rights reserved.
 			</p>
 		</div>
 	);
