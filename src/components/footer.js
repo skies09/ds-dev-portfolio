3d961d104518e3766b927b3cc6716c5f0175a6f5
Donna <skies09@hotmail.com>
2025-07-18 13:25:16 +0100

    add animated gradients

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
 	return (
-		<div className="flex flex-row w-full justify-start items-center py-4 bg-gradient-to-b from-colorTwo to-colorThree border-t-2 border-dotted border-colorFive">
+		<div className="flex flex-row w-full justify-start items-center py-4 bg-gradient-to-b from-colorTwo to-colorThree border-t-2 gradient-background border-dotted border-colorFive">
 			<p className="text-colorFive text-sm pl-4 z-10">
 				© 2025{" "}
 				<span className="font-bold hover:animate-wiggle">
