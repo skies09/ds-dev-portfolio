fb1eb733d5d3f90d6cb309d25e7556e101e4f009
Donna <skies09@hotmail.com>
2025-12-24 16:18:08 +0000

    update social links designs

--- src/components/socialLinks.js
+++ src/components/socialLinks.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { motion } from "framer-motion";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
 	faFacebook,
@@ -8,36 +9,60 @@ import {
 } from "@fortawesome/free-brands-svg-icons";
 
 const SocialLinks = () => {
+	const socialLinks = [
+		{
+			id: "facebook",
+			href: "https://www.facebook.com/profile.php?id=100002920786337",
+			icon: faFacebook,
+			label: "Facebook",
+		},
+		{
+			id: "instagram",
+			href: "https://www.instagram.com/skies009/",
+			icon: faInstagram,
+			label: "Instagram",
+		},
+		{
+			id: "github",
+			href: "https://github.com/skies09",
+			icon: faGithub,
+			label: "GitHub",
+		},
+		{
+			id: "linkedIn",
+			href: "https://www.linkedin.com/in/donna-smith-091285/",
+			icon: faLinkedin,
+			label: "LinkedIn",
+		},
+	];
+
 	return (
-		<div className="flex flex-row justify-center items-center pt-6 mb-8">
-			<a
-				id="facebook"
-				href="https://www.facebook.com/profile.php?id=100002920786337"
-				className="mx-3 inline-block text-lg text-colorOne hover:text-colorFive"
-			>
-				<FontAwesomeIcon icon={faFacebook} size="2x" />
-			</a>
-			<a
-				id="instagram"
-				href="https://www.instagram.com/skies009/"
-				className="mx-3 inline-block text-lg text-colorOne hover:text-colorFive"
-			>
-				<FontAwesomeIcon icon={faInstagram} size="2x" />
-			</a>
-			<a
-				id="github"
-				href="https://github.com/skies09"
-				className="mx-3 inline-block text-lg text-colorOne hover:text-colorFive"
-			>
-				<FontAwesomeIcon icon={faGithub} size="2x" />
-			</a>
-			<a
-				id="linkedIn"
-				href="https://www.linkedin.com/in/donna-smith-091285/"
-				className="mx-3 inline-block text-lg text-colorOne hover:text-colorFive"
-			>
-				<FontAwesomeIcon icon={faLinkedin} size="2x" />
-			</a>
+		<div className="flex flex-row justify-center items-center pt-6 mb-8 gap-3 lg:gap-4">
+			{socialLinks.map((link, index) => (
+				<motion.a
+					key={link.id}
+					id={link.id}
+					href={link.href}
+					target="_blank"
+					rel="noopener noreferrer"
+					aria-label={`Visit ${link.label}`}
+					className="relative p-3 rounded-xl bg-colorTwo text-colorFive hover:text-colorOne transition-all duration-300 shadow-neo-outset hover:shadow-neo-inset"
+					whileHover={{ scale: 1.05 }}
+					whileTap={{ scale: 0.98 }}
+					initial={{ opacity: 0, scale: 0.8 }}
+					animate={{ opacity: 1, scale: 1 }}
+					transition={{
+						duration: 0.5,
+						delay: index * 0.1,
+						ease: "easeOut",
+					}}
+				>
+					<FontAwesomeIcon
+						icon={link.icon}
+						className="text-xl lg:text-2xl relative z-10"
+					/>
+				</motion.a>
+			))}
 		</div>
 	);
 };
